d4030e0de95c5debe5e7101970b989d0fe97d5ad
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-09-30 17:34:11 +0530

    Router done

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-import Store from "./services/Store";
-import API from "./services/API";
-import { loadData } from "./services/Menu";
+import Store from "./services/Store.js";
+import API from "./services/API.js";
+import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 window.app = {};
 app.store = Store;
-window.addEventListener("DOMContentLoaded", () => {
+app.router = Router;
+//* It's better to wait for the event for manipulation.
+window.addEventListener("DOMContentLoaded", async () => {
   loadData();
+  app.router.init();
 });
