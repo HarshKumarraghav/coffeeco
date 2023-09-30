04fbfb7a9234959300de742fcba02dc6067934d9
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-09-30 14:10:21 +0530

    add few service

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-  
+import Store from "./services/Store";
+import API from "./services/API";
+import { loadData } from "./services/Menu";
+window.app = {};
+app.store = Store;
+window.addEventListener("DOMContentLoaded", () => {
+  loadData();
+});
